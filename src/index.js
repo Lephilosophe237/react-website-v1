b05c529d006198e81d830c1c7738bf51f39c2206
Lephilosophe237 <jngan077@uottawa.ca>
2023-07-20 16:16:51 -0400

    ffg

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './i18n.js';
-ReactDOM.render(<Suspense fallback="Loading..."><App /></Suspense>, document.getElementById('root'));
